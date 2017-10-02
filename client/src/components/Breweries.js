aee18236bcc592efadf4fb818a1882dc44c7e4f9
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-10-01 18:43:03 -0600

    Add images to breweries

--- client/src/components/Breweries.js
+++ client/src/components/Breweries.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, Divider } from 'semantic-ui-react';
+import { Card, Divider,Image } from 'semantic-ui-react';
 import axios from 'axios';
+import altimage from '../images/image.png';
 
 class Breweries extends React.Component {
   state = { Breweries: [] }
@@ -8,22 +9,25 @@ class Breweries extends React.Component {
   componentDidMount() {
     axios.get('/api/all_breweries')
       .then( ({ data }) => {
+        console.log(data.entries)
         this.setState({ Breweries: data.entries })
       });
   }
 
   render() {
     let { Breweries } = this.state;
+    
     return (
       <div>
         <Divider />
           <Card.Group>    
-              { Breweries.map( Brewery =>
-                  <Card key={Brewery.id} >
+              { Breweries.map( ({id,name,images={}}) => 
+                  <Card key={id}>
                       <Card.Content>
+                      <Image src={images.icon} alt={'Brewery Logo'}/>
                         <Divider />
                         <Card.Header>
-                          {Brewery.name}
+                          {name}
                         </Card.Header>
                       </Card.Content>
                   </Card>
