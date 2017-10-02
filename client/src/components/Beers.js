75b09255bdd03edc6dbbc81b241ab90aab0be414
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-10-01 23:35:20 -0600

    Add ugly Beer pagination

--- client/src/components/Beers.js
+++ client/src/components/Beers.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Card, Divider } from 'semantic-ui-react';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
+import '../styles/pagination.css';
 
 class Beers extends React.Component {
-  state = { beers: [] }
+  state = { beers: [], page: 1, pageCount: 0, offset: 10, per_page: 10 }
 
-  componentDidMount() {
-    axios.get('/api/all_beers')
-      .then( ({ data }) => {
-        this.setState({ beers: data.entries })
-      });
+  componentDidMount() { 
+    this.loadPage();
+  }
+  loadPage() { 
+
+    axios.get(`/api/all_beers?page=${this.state.page}&per_page=${this.state.per_page}`)
+    .then( ({ data }) => {
+      this.setState({ beers: data.entries, pageCount: data.total_pages})
+    });
   }
 
+  handlePageClick = (beers) => {
+    let selected = beers.selected;
+    let offset = Math.ceil(selected * this.state.per_page);
+    let per_page = this.state.per_page;
+    let page = Math.ceil(offset/per_page)
+    this.setState({offset: offset, page: page}, () => {
+      this.loadPage();
+    });
+};
   render() {
     let { beers } = this.state;
     return (
@@ -30,6 +45,21 @@ class Beers extends React.Component {
                   </Card>
               )};            
           </Card.Group>
+          <div>
+            <ReactPaginate previousLabel={"previous"}
+                        nextLabel={"next"}
+                        breakLabel={<a href="">...</a>}
+                        breakClassName={"break-me"}
+                        pageCount={this.state.pageCount}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={5}
+                        onPageChange={this.handlePageClick}
+                        containerClassName={"react-paginate"}
+                        subContainerClassName={"react-paginate"}
+                        activeClassName={"active"} 
+                        pageClassName={"pageClass"}
+                        />
+          </div>
         </div>
     )
   }
